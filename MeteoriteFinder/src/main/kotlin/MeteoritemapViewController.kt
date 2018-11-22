80185fe5b96313f141fc98901155bbb584293b63
MHO <mauricio.hdez.o.10@gmail.com>
2018-11-22 15:12:13 -0600

    Fleshing out the ViewController

--- MeteoriteFinder/src/main/kotlin/MeteoritemapViewController.kt
+++ MeteoriteFinder/src/main/kotlin/MeteoritemapViewController.kt
@@ -2,6 +2,7 @@
 import kotlinx.cinterop.ExportObjCClass
 import kotlinx.cinterop.ObjCObjectBase.OverrideInit
 import kotlinx.cinterop.ObjCOutlet
+import platform.CoreLocation.CLLocationCoordinate2DMake
 import platform.Foundation.*
 import platform.UIKit.*
 import platform.MapKit.*
@@ -14,8 +15,36 @@ class MeteoriteMapViewController : UIViewController, MKMapViewDelegateProtocol {
     @OverrideInit
     constructor(coder: NSCoder) : super(coder)
 
+    @ObjCOutlet
+    lateinit var mapView: MKMapView
+
     // 4 Overrides the viewDidLoad() method.
     override fun viewDidLoad() {
         super.viewDidLoad()
+
+        val center = CLLocationCoordinate2DMake(38.8935754, -77.0847873)
+        val span = MKCoordinateSpanMake(0.7, 0.7)
+        val region = MKCoordinateRegionMake(center, span)
+
+        with(mapView){
+            delegate = this@MeteoriteMapViewController
+            setRegion(region, true)
+        }
+
+        createAnnotation()
+    }
+
+    override fun mapViewDidFailLoadingMap(mapView: MKMapView, withError: NSError) {
+        NSLog("Error loading map: $withError")
+    }
+
+    private fun createAnnotation(){
+        val annotation = MKPointAnnotation().apply {
+            setCoordinate(CLLocationCoordinate2DMake(38.8935754, -77.0847873))
+            setTitle("My mock meteorite")
+            setSubtitle("I'm falling........")
+        }
+
+        mapView.addAnnotation(annotation)
     }
 }
